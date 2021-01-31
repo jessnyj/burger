fc593ef80b6998839f1e3c36e6b31e4a273cb220
jessny <jessnyjoseph@gmail.com>
2021-01-30 20:52:38 -0800

    created schema with table

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE database burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(30),
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);

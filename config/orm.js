4ed8d1cf79e6184d7ebab58b6edb721a50a3ace1
jessny <jessnyjoseph@gmail.com>
2021-01-31 14:16:53 -0800

    created orm file with initial functions

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+function selectAll() {
+
+}
+
+function insertOne() {
+
+}
+
+function updateOne() {
+    
+}
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;

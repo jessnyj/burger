fce9efc154ae84271fb3b0d674e1809424562a47
jessny <jessnyjoseph@gmail.com>
2021-01-30 21:34:53 -0800

    created seeds, added to workbench

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", FALSE);

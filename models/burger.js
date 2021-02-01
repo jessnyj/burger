9b122f9ffca2c072178cb196b4b4dd3a1ef1a821
jessny <jessnyjoseph@gmail.com>
2021-01-31 19:40:18 -0800

    added routes

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,19 @@
 var orm = require("../config/orm.js");
 
-
-// Export the database functions for the controller (burger.js).
-// module.exports = burger;
+const burger = {
+    all(cb) {
+        orm.all('cats', (res) => cb(res));
+    },
+    // The variables cols and vals are arrays.
+    create(cols, vals, cb) {
+        orm.create('cats', cols, vals, (res) => cb(res));
+    },
+    update(objColVals, condition, cb) {
+        orm.update('cats', objColVals, condition, (res) => cb(res));
+    },
+    delete(condition, cb) {
+        orm.delete('cats', condition, (res) => cb(res));
+    },
+};
+// Export the database functions for the controller (burger_controller.js).
+module.exports = burger;

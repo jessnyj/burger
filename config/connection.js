77efcb93b28338a5392c714ef8caf5b734651fdc
jessny <jessnyjoseph@gmail.com>
2021-02-01 22:22:45 -0800

    deploy link

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
-
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.host || 'localhost',
     port: 3306,
-    user: 'root',
-    // NOTE: Be sure to add your MySQL password here!
-    password: 'password',
-    database: 'burgers_db',
+    user: process.env.user || 'root',
+    // NOTE: Be sure to add your MySQL password here! we defaulted to : password
+    password: process.env.password || 'password',
+    database: process.env.db || 'burgers_db',
 });
-
 // Make connection.
 connection.connect((err) => {
     if (err) {
@@ -18,6 +16,5 @@ connection.connect((err) => {
     }
     console.log(`connected as id ${connection.threadId}`);
 });
-
 // Export connection for our ORM to use.
 module.exports = connection;
--- public/assets/js/burger_buttons.js
+++ public/assets/js/burger_buttons.js
@@ -3,6 +3,43 @@ document.addEventListener('DOMContentLoaded', (event) => {
         console.info('DOM loaded');
     }
 
+    // UPDATE
+    const changeSleepBtns = document.querySelectorAll('.change-sleep');
+
+    // Set up the event listener for the create button
+    if (changeSleepBtns) {
+        changeSleepBtns.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                // Grabs the id of the element that goes by the name, "id"
+                const id = e.target.getAttribute('data-id');
+                const newSleep = e.target.getAttribute('data-newsleep');
+
+                const newSleepState = {
+                    sleepy: newSleep,
+                };
+
+                fetch(`/api/cats/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+
+                    // make sure to serialize the JSON body
+                    body: JSON.stringify(newSleepState),
+                }).then((response) => {
+                    // Check that the response is all good
+                    // Reload the page so the user can see the new quote
+                    if (response.ok) {
+                        console.log(`changed sleep to: ${newSleep}`);
+                        location.reload('/');
+                    } else {
+                        alert('something went wrong!');
+                    }
+                });
+            });
+        });
+    }
     const createBurgerBtn = document.getElementById('create-form');
 
     if (createBurgerBtn) {
